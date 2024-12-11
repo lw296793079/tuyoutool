e746a8a8e1e214eaf7f02c4aa5a76527c787a7b9
lw296793079 <296793079@qq.com>
2024-12-11 19:46:26 +0800

    Next.js 配置文件格式不支持next.config.ts

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // 如果你有其他配置，也放在这里
+}
+
+module.exports = nextConfig 
